3d545a8178a0a5a37f1d78bd8ae25d7dbc7f387f
Alexander Pronin <pronin.alx@gmail.com>
2014-03-11 19:57:29 +0200

    Initial Commit

---
+++ 2302GesturesTestHW/RITViewController.h
@@ -0,0 +1,13 @@
+//
+//  RITViewController.h
+//  2302GesturesTestHW
+//
+//  Created by Pronin Alexander on 11.03.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITViewController : UIViewController
+
+@end
